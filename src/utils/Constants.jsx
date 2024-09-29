5cee8cb63e22d69e1cbcdc9bb9a82c4c64723027
Roshan Basu <roshanbasu7@gmail.com>
2024-09-29 11:20:03 +0530

    Using env variables

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,1 +1,1 @@
-export const API_HOST = process.env.nonLocal === "false" ? "localhost" : "3.110.134.192";
+export const API_HOST = process.env.nonLocal === "false" ? "localhost" : process.env.apiHost;
