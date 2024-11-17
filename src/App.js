09e1b5f6acd96f07d5ab22768c54320d2eb096b6
Roshan Basu <roshanbasu7@gmail.com>
2024-11-17 22:39:38 +0530

    Route correction

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<ChatbotList />} />
-                <Route path="/chatbots" element={<Login />} />
+                <Route path="/chatbots" element={<ChatbotList />} />
                 <Route path="/workflows/:workflowId" element={
                   <WorkflowForm />
                 } />
