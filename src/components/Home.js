fc551f27697ae71393bc0bbd66c7ee28d8aeb127
jaganathkrishnan <147528350+jaganathkrishnan@users.noreply.github.com>
2024-12-08 11:14:38 +0530

    landing page added sections and doodle in bg

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
-import './../styling/Home.css'
+import './../styling/Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div className="home-container">
-        <h1>Welcome to Apartix</h1>
-        <p>This is a simple homepage styled like LinkedIn.</p>
-      </div>
+        {/* Section 1: Welcome */}
+        <div className='home1-container'>
+          <div className='home1-para'>
+          <h1>Welcome to <br />Apartix</h1>
+          </div>
+          <div className='home2-container'>
+            <p>Welcome to Apartix, where managing your property-related tasks has never been easier. </p>
+          </div>
+        </div>
+
+        {/* Section 2: Features */}
+        <div className='home3-container'>
+          <h2>What we do:</h2>
+          <div className='home3-content'>
+            <p>Our platform offers a wide range of features designed to help you manage your property effectively, including:</p>
+            
+          </div>
+        </div>
+
+        {/* Section 3: Contact Us */}
+        <div className='home4-container'>
+          <h2>Know more</h2>
+          <div className='home4-content'>
+            <p>Have any questions or need assistance? Get in touch with us:</p>
+            <div className='cards'>
+              <div className='card'>
+                <h3>For Management committee</h3></div>
+              <div className='card'>
+                <h3>For Residents</h3></div>
+              <div className='card'>
+                <h3>For Vendor employees</h3></div>
+              <div className='card'>
+                <h3>For property owners</h3></div>
+              </div>
+              </div>
+              </div>
+              </div>
+           
     );
   }
 }
